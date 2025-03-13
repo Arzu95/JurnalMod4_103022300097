65c7f0acda0cec6e360039ca673adfd83f2f4daa
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-13 11:06:51 +0700

    menambahkan class FanLaptop

---
+++ FanLaptop.cs
@@ -0,0 +1,55 @@
+﻿namespace JurnalMod4_103022300097
+{
+    public class FanLaptop
+    {
+        public enum State { Quiet, Balanced, Performanced, Turbo }
+        public enum Mode { ModeUp, ModeDown, TurboShorcut }
+
+        public class transition
+        {
+            public State prevState;
+            public State nextState;
+            public Mode mode;
+            public transition(State prevState, State nextState, Mode mode)
+            {
+                this.prevState = prevState;
+                this.nextState = nextState;
+                this.mode = mode;
+            }
+        }
+        public State currentState = State.Quiet;
+        public State getNextState(State prevState, Mode mode)
+        {
+            State state = prevState;
+            for (int i = 0; i < transitions.Length; i++)
+            {
+                if (transitions[i].prevState == prevState && transitions[i].mode == mode)
+                {
+                    return transitions[i].nextState;
+                }
+            }
+            return prevState;
+        }
+
+        public void activateMode(Mode mode)
+        {
+            Console.Write("Fan: " + currentState);
+            State nextState = getNextState(currentState, mode);
+            currentState = nextState;
+            Console.WriteLine(" Berubah menjadi " + currentState);
+        }
+
+        transition[] transitions =
+        {
+            new transition(State.Quiet, State.Balanced, Mode.ModeUp ),
+            new transition(State.Balanced, State.Performanced, Mode.ModeUp ),
+            new transition(State.Performanced, State.Turbo, Mode.ModeUp ),
+            new transition(State.Turbo, State.Quiet, Mode.TurboShorcut ),
+            new transition(State.Quiet, State.Turbo, Mode.TurboShorcut ),
+            new transition(State.Turbo, State.Performanced, Mode.ModeDown ),
+            new transition(State.Performanced, State.Balanced, Mode.ModeDown ),
+            new transition(State.Balanced, State.Quiet, Mode.ModeDown ),
+        };
+    }
+}
+
---
+++ KodeProduk.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace  JurnalMod4_103022300097
+{
+    class KodeProduk
+    {
+        private static readonly Dictionary<string, string> kodeProDict = new Dictionary<string, string>()
+    {
+        {"Laptop", "E100"},
+        {"Smartphone", "E101"},
+        {"Tablet", "E102"},
+        {"Headset", "E103"},
+        {"Keyboard", "E104"},
+        {"Mouse", "E105"},
+        {"Printer", "E106"},
+        {"Monitor", "E107"},
+        {"Smartwatch", "E108"},
+        {"Kamera", "E109"}
+    };
+        public static string getKodeProduk(string namaProduk)
+        {
+            if (kodeProDict.ContainsKey(namaProduk))
+            {
+                return kodeProDict[namaProduk];
+            }
+            else
+            {
+                return "Produk tidak ditemukan";
+            }
+        }
+    }
+}
+
